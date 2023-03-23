047d65d3abc99895e884759edf2b17de02ea7bbb
Pablo Peñaloza <corsario666@gmail.com>
2023-03-22 20:47:03 -0400

    ejercicio en clases 3

---
+++ Trabajo en clases/Practica 2/ordpalabras.php
@@ -0,0 +1,14 @@
+<?php
+    $cadena=$_GET['cadena'];
+    $palabras=explode(" ", $cadena);
+    foreach($palabras as $palabra)
+    {
+        ?>
+        <div style ="text-align:center;"><?php
+            echo strtoupper($palabra[0]).strtolower(substr($palabra,1));
+        ?>
+
+        </div><?php
+    }
+
+?>
